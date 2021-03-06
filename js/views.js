c67faa0ffd4d5e4eaec9347d2ffde3adb367b1f8
Jorge Salazar <j-salazarp@hotmail.com>
2021-03-06 23:26:15 +0100

    Customizamos los anuncios

--- js/views.js
+++ js/views.js
@@ -5,39 +5,103 @@ export const adView = (ad) => {
   
   let deleteButtonHTML = '';
 
-  // if(ad.canBeDeleted){
-  //   deleteButtonHTML = '<button class="button is-danger">Delete</button>';
-  // }
+  if(ad.canBeDeleted){
+    deleteButtonHTML = '<button class="button is-danger">Delete</button>';
+  }
 
   let imgHTML = '';
 
   
-  if(ad.image){
+//   if(ad.image){
     
-    imgHTML = `<div class="card-image">
-    <figure class="image">
-    <img src="${ad.image}" alt="Placeholder image">
-    </figure>
-    </div>`;
+//     imgHTML = `<div class="card-image">
+//     <figure class="image">
+//     <img src="${ad.image}" alt="Placeholder image">
+//     </figure>
+//     </div>`;
 
-  }
-  console.log(ad);
-    return `<div class="card">
-      ${imgHTML}
-      <div class="card-content">
-        <div class="media">
-          <div class="media-content">
-            <p class="title is-4">${ad.name}</p>
-          </div>
-        </div>
-        <div class="content price" name="price">${ad.price}€</div>
-        <div class="content buy">${ad.buy}</div>
-        
-      </div>
-     
-    </div>`;
+//   }
+ 
+//   return `<article class="column">
+//   <div class="card">
+//   ${imgHTML}
+//       <div class="card-content">
+//       <div class="media">
+//         <div class="media-content">
+//           <p class="title is-4">${ad.name}</p>
+//         </div>
+//       </div>
+//       <div class="content price" name="price">${ad.price}€</div>
+//       <div class="content buy">${ad.buy}</div>
+//       <div class="content">
+//                 <div class="placeholder"></div>
+//                 <div class="placeholder"></div>
+//                 <div class="placeholder"></div>
+//                 <div class="placeholder"></div>
+//             </div>
+//   </div>
+// </article>`;
+
+
+
+
+return `<div class="card">
+<div class="card-image">
+  <figure class="image is-4by3">
+    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+  </figure>
+</div>
+<div class="card-content">
+  <div class="media">
+    <div class="media-left">
+      <figure class="image is-48x48">
+        <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+      </figure>
+    </div>
+    <div class="media-content">
+      <p class="title is-4">John Smith</p>
+      <p class="subtitle is-6">@johnsmith</p>
+    </div>
+  </div>
+
+  <div class="content">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+    <a href="#">#css</a> <a href="#">#responsive</a>
+    <br>
+    <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+  </div>
+</div>
+</div>`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 };
+
+
+
 export const errorView = (errorMessage) => {
 
     return ` <article class="message is-danger">
@@ -57,6 +121,7 @@ export const errorView = (errorMessage) => {
 export const adViewDetail = (detailAd) => {
 
 
+  
   let deleteButtonHTML = '';
 
   if(detailAd.canBeDeleted){
@@ -88,9 +153,7 @@ export const adViewDetail = (detailAd) => {
         <div class="content buy">${detailAd.buy}</div>
         
       </div>
-      ${deleteButtonHTML}
      
     </div>`;
 
 };
-
