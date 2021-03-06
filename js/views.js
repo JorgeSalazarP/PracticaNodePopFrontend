dd0dfa9a28bf4f36686449223d949fff99fc2138
Jorge Salazar <j-salazarp@hotmail.com>
2021-03-06 23:33:41 +0100

    Modificamos el tamaño de la tarjetas

--- js/views.js
+++ js/views.js
@@ -12,15 +12,15 @@ export const adView = (ad) => {
   let imgHTML = '';
 
   
-//   if(ad.image){
+  if(ad.image){
     
-//     imgHTML = `<div class="card-image">
-//     <figure class="image">
-//     <img src="${ad.image}" alt="Placeholder image">
-//     </figure>
-//     </div>`;
+    imgHTML = `<div class="card-image">
+    <figure class="image">
+    <img src="${ad.image}" alt="Placeholder image">
+    </figure>
+    </div>`;
 
-//   }
+  }
  
 //   return `<article class="column">
 //   <div class="card">
@@ -46,32 +46,16 @@ export const adView = (ad) => {
 
 
 return `<div class="card">
-<div class="card-image">
-  <figure class="image is-4by3">
-    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-  </figure>
-</div>
+${imgHTML}
 <div class="card-content">
-  <div class="media">
-    <div class="media-left">
-      <figure class="image is-48x48">
-        <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
-      </figure>
-    </div>
-    <div class="media-content">
-      <p class="title is-4">John Smith</p>
-      <p class="subtitle is-6">@johnsmith</p>
-    </div>
-  </div>
-
-  <div class="content">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-    <a href="#">#css</a> <a href="#">#responsive</a>
-    <br>
-    <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-  </div>
-</div>
+<div class="card-content">
+      <div class="media">
+        <div class="media-content">
+          <p class="title is-4">${ad.name}</p>
+        </div>
+      </div>
+      <div class="content price" name="price">${ad.price}€</div>
+      <div class="content buy">${ad.buy}</div>
 </div>`;
 
 
